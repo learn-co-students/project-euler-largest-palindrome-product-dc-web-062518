ad614bad4439ebc66566b0635b27db3d818fe15d
github username <github email address>
2018-09-21 10:03:49 -0400

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,18 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def answer
+    largest = 0
+    (2..999).to_a.each do |num1|
+      (1..999).to_a.each do |num2|
+        int = num1 * num2
+        pal = int.to_s
+        if int > largest && pal === pal.reverse
+          largest = int
+        end
+      end
+    end
+    return largest
+  end
+  
+end
